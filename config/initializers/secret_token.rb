3efd9aaf92b3a0d140641a7288b58d90654a84ac
josephine <josephine.nakka@yahoo.com>
2014-09-12 12:37:22 -0700

    fitst commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Index::Application.config.secret_token = '900b17a0959baeabc33883fe64c03f668f895d65a7f7025d142a4ed1025480cc4c608ea97dc88ffcacf7dc92ccfc187d2cb5cb23a31ae42b387f34ee6e820c6b'
